5433069753cc48faef03571d8f1cc508701a9a86
Nazmul Hassan <hnazmul748@gmail.com>
2023-05-02 00:57:28 +0600

    first json folder create and setup

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const service = require("./data/service.json")
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.get('/service', (req, res) => {
+    res.send(service)
 })
   
 app.listen(port, () => {
