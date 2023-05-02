fc2f02e1d90164971303e9725a75dbe3e2de870a
Nazmul Hassan <hnazmul748@gmail.com>
2023-05-02 21:36:07 +0600

    new cheff.json file has been added

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const service = require("./data/service.json")
+const service = require("./data//service.json")
+const cheff = require("./data/cheff.json")
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get("/service", (req, res) => {
+    res.send(service)
 })
 
-app.get('/service', (req, res) => {
-    res.send(service)
+app.get("/cheff", (req, res) => {
+    res.send(cheff)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
   
 app.listen(port, () => {
